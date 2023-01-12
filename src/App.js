32c6529dbe71c9140b10ecac507770ecbcd59eca
LiuLuchik <114955848+LiuLuchik@users.noreply.github.com>
2023-01-12 01:49:50 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        <a
-          className="App-link"
-          href="https://github.com/LiuLuchik/weather-app-react-liu"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Git
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App </h1>
+        <footer>
+          <a
+            className="App-link"
+            href="https://github.com/LiuLuchik/weather-app-react-liu"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Git
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
