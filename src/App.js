a14817d6405bb8a83d273d207ff56597c88c57cf
LiuLuchik <114955848+LiuLuchik@users.noreply.github.com>
2023-01-12 01:21:41 +0000

    create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h1>Weather App </h1>
+      <footer>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/LiuLuchik/react-weather-app-learn"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Git
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
